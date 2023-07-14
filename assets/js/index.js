ffaf752252b335af805f0312be3a0e41f7675f08
Matthew Rich Marin <matthewrichmarin@gmail.com>
2023-07-14 16:28:19 +0800

    adding search features in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,7 @@
 import { sidebar } from './sidebar.js';
 import { api_key, imageBaseURL, fetchDataFromServer } from './api.js';
 import { createMovieCard} from './movie-card.js';
+import { search } from './search.js';
 
 const pageContent = document.querySelector('[page-content]');
 
